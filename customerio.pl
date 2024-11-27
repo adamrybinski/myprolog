959f78d3516fad7356bf6068239ed5eb4c5d0965
adamrybinski <adam@compose.land>
2024-11-27 17:01:42 +0100

    Clean customer

--- customerio.pl
+++
@@ -1,9 +0,0 @@
-% main.pl
-:- [customeriostate].
-:- [validation_rules].
-:- [dead_end_detection].
-:- [detectloops].
-
-% Automatically execute when file is loaded
-:- print_path_to_dead_end(user_activity).
-:- find_dead_ends(DeadEnds), write('Dead ends found: '), write(DeadEnds), nl.
